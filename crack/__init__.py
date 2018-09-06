0afe95b22e210dae614d7b05b68d95e0b68e01fc
jelleas <jelle.van.assema@gmail.com>
2018-09-06 15:29:12 +0200

    no 4 letter checks in crack

--- crack/__init__.py
+++ crack/__init__.py
@@ -21,25 +21,25 @@ def cracks_jason():
     """cracks jason's password: 50YHuxoCN9Jkc"""
     check50.run("./crack 50YHuxoCN9Jkc").stdout("JH", timeout=20).exit(0)
 
-@check50.check(compiles)
-def cracks_malan():
-    """cracks malan's password: 50QvlJWn2qJGE"""
-    check50.run("./crack 50QvlJWn2qJGE").stdout("NOPE", timeout=20).exit(0)
+# @check50.check(compiles)
+# def cracks_malan():
+#     """cracks malan's password: 50QvlJWn2qJGE"""
+#     check50.run("./crack 50QvlJWn2qJGE").stdout("NOPE", timeout=20).exit(0)
 
 @check50.check(compiles)
 def cracks_mzlatkova():
     """cracks mzlatkova's password: 50CPlMDLT06yY"""
     check50.run("./crack 50CPlMDLT06yY").stdout("ha", timeout=20).exit(0)
 
-@check50.check(compiles)
-def cracks_patrick():
-    """cracks patrick's password: 50WUNAFdX/yjA"""
-    check50.run("./crack 50WUNAFdX/yjA").stdout("Yale", timeout=20).exit(0)
+# @check50.check(compiles)
+# def cracks_patrick():
+#     """cracks patrick's password: 50WUNAFdX/yjA"""
+#     check50.run("./crack 50WUNAFdX/yjA").stdout("Yale", timeout=20).exit(0)
 
-@check50.check(compiles)
-def cracks_rbowden():
-    """cracks rbowden's password: 50fkUxYHbnXGw"""
-    check50.run("./crack 50fkUxYHbnXGw").stdout("rofl", timeout=20).exit(0)
+# @check50.check(compiles)
+# def cracks_rbowden():
+#     """cracks rbowden's password: 50fkUxYHbnXGw"""
+#     check50.run("./crack 50fkUxYHbnXGw").stdout("rofl", timeout=20).exit(0)
 
 @check50.check(compiles)
 def cracks_summer():
@@ -51,10 +51,10 @@ def cracks_stelios():
     """cracks stelios's password: 50nq4RV/NVU0I"""
     check50.run("./crack 50nq4RV/NVU0I").stdout("ABC", timeout=20).exit(0)
 
-@check50.check(compiles)
-def cracks_wmartin():
-    """cracks wmartin's password: 50vtwu4ujL.Dk"""
-    check50.run("./crack 50vtwu4ujL.Dk").stdout("haha", timeout=20).exit(0)
+# @check50.check(compiles)
+# def cracks_wmartin():
+#     """cracks wmartin's password: 50vtwu4ujL.Dk"""
+#     check50.run("./crack 50vtwu4ujL.Dk").stdout("haha", timeout=20).exit(0)
 
 @check50.check(compiles)
 def cracks_zamyla():
