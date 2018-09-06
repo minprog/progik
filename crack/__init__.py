329c1784facc700f8c478f82b42722a14f3d2f48
jelleas <jelle.van.assema@gmail.com>
2018-09-06 15:00:41 +0200

    crack start

--- crack/__init__.py
+++ crack/__init__.py
@@ -1,8 +1,17 @@
 import check50
-
-# Coming soon...
+import check50.c
 
 @check50.check()
-def submitted():
-    """crack submitted"""
-    pass
+def exists():
+    """crack.c exists."""
+    check50.exists("crack.c")
+
+@check50.check(exists)
+def compiles():
+    """crack.c compiles."""
+    check50.c.compile("crack.c", lcs50=True)
+
+@check50.check(compiles)
+def cracks_andi():
+    """cracks andi's password: 50.jPgLzVirkc"""
+    check50.run("./crack", "50.jPgLzVirkc").stdout("hi").exit(0)
