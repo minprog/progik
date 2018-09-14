306b7a592e6f600ee36b12b49ffc4208dbb6f41a
jelleas <jelle.van.assema@gmail.com>
2018-09-14 17:30:33 +0200

    sort_reversed

--- find/less/__init__.py
+++ find/less/__init__.py
@@ -6,6 +6,7 @@ def exists():
     """helpers.c exists."""
     check50.exists("helpers.c")
 
+
 @check50.check(exists)
 def compiles():
     """helpers.c compiles."""
@@ -13,20 +14,12 @@ def compiles():
     check50.c.compile("sort.c", "helpers.c", exe_name="sort", lcs50=True)
     check50.c.compile("find.c", "helpers.c", exe_name="find", lcs50=True)
 
-# def test_sorted(self, items):
-#     p = self.spawn("./sort")
-#     for i in items:
-#         p.stdin(str(i))
-#     p.stdin(EOF)
-#     for i in sorted(items):
-#         p.stdout(str(i))
-#     p.exit(0)
-#
-# @check("compiles")
-# def sort_reversed(self):
-#     """sorts {5,4,3,2,1}"""
-#     self.test_sorted([5, 4, 3, 2, 1])
-#
+
+@check50.check(compiles)
+def sort_reversed():
+    """sorts {5,4,3,2,1}"""
+    test_sorted([5, 4, 3, 2, 1])
+
 # @check("compiles")
 # def sort_shuffled(self):
 #     """sorts {5,3,1,2,4,6}"""
@@ -86,3 +79,12 @@ def compiles():
 # def correctly_sorts(self):
 #     """finds 28 in {30,27,28,26}"""
 #     self.spawn("./find 28").stdin("30").stdin("27").stdin("28").stdin("26").stdin(EOF).exit(0)
+
+def test_sorted(items):
+    check = check50.run("./sort")
+    for i in items:
+        check.stdin(str(i))
+    check.stdin(check50.EOF)
+    for i in sorted(items):
+        check.stdout(str(i))
+    check.exit(0)
