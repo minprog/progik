134ab70afd29e63792b18393c047c1b436357821
jelleas <jelle.van.assema@gmail.com>
2018-09-14 16:43:30 +0200

    simplification

--- fifteen/__init__.py
+++ fifteen/__init__.py
@@ -17,76 +17,27 @@ def compiles():
 @check50.check(compiles)
 def init3():
     """initializes a 3x3 board correctly."""
-    check50.run("./fifteen 3") \
-        .stdout("8").stdout("7").stdout("6") \
-        .stdout("5").stdout("4").stdout("3") \
-        .stdout("2").stdout("1").stdout("[_0]") \
-        .stdout("\n") \
+    board = ["8", "7", "6",
+             "5", "4", "3",
+             "2", "1", "[_0]"]
+
+    check = check50.run("./fifteen 3")
+    for tile in board:
+        check.stdout("tile")
+    check.stdout("\n")
 
 @check50.check(compiles)
 def init4():
     """initializes a 4x4 board correctly."""
-    check50.run("./fifteen 4") \
-        .stdout("15").stdout("14").stdout("13").stdout("12") \
-        .stdout("11").stdout("10").stdout("9").stdout("8") \
-        .stdout("7").stdout("6").stdout("5").stdout("4") \
-        .stdout("3").stdout("1").stdout("2").stdout("[_0]") \
-        .stdout("\n") \
-
-@check50.check(compiles)
-def solve3():
-    """solves a 3x3 board."""
-    steps = ["3","4","1","2","5","8","7","6",
-             "4","1","2","5","8","7","6","4",
-             "1","2","4","1","2","3","5","4",
-             "7","6","1","2","3","7","4","8",
-             "6","4","8","5","7","8","5","6",
-             "4","5","6","7","8","6","5","4",
-             "7","8"]
-
-    check = check50.run("./fifteen 3")
-
-    for step in steps:
-        check.stdin(step, prompt=False)
-
-    check.stdout("1").stdout("2").stdout("3") \
-         .stdout("4").stdout("5").stdout("6") \
-         .stdout("7").stdout("8").stdout("[_0]") \
-         .stdout("\n")
-
-@check50.check(compiles)
-def solve4():
-    """solves a 4x4 board."""
-    steps = ["4","5","6","1","2","4","5","6",
-             "1","2","3","7","11","10","9","1",
-             "2","3","4","5","6","8","1","2",
-             "3","4","7","11","10","9","14",
-             "13","12","1","2","3","4","14",
-             "13","12","1","2","3","4","14",
-             "13","12","1","2","3","4","12",
-             "9","15","1","2","3","4","12","9",
-             "13","14","9","13","14","7","5",
-             "9","13","14","15","10","11","5",
-             "9","13","7","11","5","9","13",
-             "7","11","15","10","5","9","13",
-             "15","11","8","6","7","8","14",
-             "12","6","7","8","14","12","6",
-             "7","8","14","15","11","10","6",
-             "7","8","12","15","11","10","15",
-             "11","14","12","11","15","10",
-             "14","15","11","12"]
+    board = ["15", "14", "13", "12",
+             "11", "10", "9", "8",
+             "7", "6", "5", "4",
+             "3", "2", "1", "[_0]"]
 
     check = check50.run("./fifteen 4")
-
-    for step in steps:
-        check.stdin(step, prompt=False)
-
-    check.stdout("1").stdout("2").stdout("3").stdout("4") \
-         .stdout("5").stdout("6").stdout("7").stdout("8") \
-         .stdout("9").stdout("10").stdout("11").stdout("12") \
-         .stdout("13").stdout("14").stdout("15").stdout("[_0]") \
-         .stdout("\n") \
-
+    for tile in board:
+        check.stdout("tile")
+    check.stdout("\n")
 
 # @check("init3")
 # def invalid8(self):
@@ -133,71 +84,63 @@ def solve4():
 #                              .stdout("8-7-6|5-4-0|2-1-3\n") \
 #                              .stdin("3")                    \
 #                              .stdout("8-7-6|5-4-3|2-1-0\n")
-#
-# @check("init3")
-# def move_around(self):
-#     """3x3 board: move up-up-left-down-down-left-up-up-right-down-down-right"""
-#     child = self.spawn("./fifteen 3").stdout("Tile to move:")
-#     moves = ["3", "6", "7", "4", "1", "2", "5", "8", "4", "1", "2", "3"]
-#     for move in moves[:-1]:
-#         child.stdin(move).stdout("Tile to move:")
-#     child.stdin(moves[-1]).stdout("4-1-7|8-2-6|5-3-0")
-#
-#
-# @check("init3")
-# def invalid_start(self):
-#     """3x3 board: make sure none of 2, 4, 5, 6, 7, 8 move tile"""
-#     child = self.spawn("./fifteen 3").stdout("Tile to move:")
-#     moves = ["2", "4", "5", "6", "7", "8"]
-#     for move in moves:
-#         child.stdin(move)                   \
-#              .stdout("Illegal move.")       \
-#              .stdout("8-7-6|5-4-3|0-2-1\n") \
-#              .stdout("Tile to move:")
-#
-# @check("init3")
-# def invalid_center(self):
-#     """3x3 board: move blank left (tile 1) then up (tile 4), then try to move tiles 1, 2, 6, 8"""
-#     child = self.spawn("./fifteen 3").stdin("1")                    \
-#                                      .stdout("Tile to move:")       \
-#                                      .stdin("4")                    \
-#                                      .stdout("8-7-6|5-0-3|2-4-1")   \
-#                                      .stdout("Tile to move:")
-#
-#     for move in ["1", "2", "6"]:
-#         child.stdin(move).stdout("Illegal move.")
-#     child.stdin("8").stdout("8-7-6|5-0-3|2-4-1")
-#
-# @check("init3")
-# def win_3x3(self):
-#     """3x3 board: make sure game is winnable"""
-#     moves = "34125876412587641241235476123748648578564567865478"
-#     child = self.spawn("./fifteen 3").stdout("Tile to move:")
-#     for move in moves[:-1]:
-#         child.stdin(move).stdout("Tile to move:")
-#     child.stdin(moves[-1]).stdout("1-2-3|4-5-6|7-8-0\n").exit(0)
-#
-# @check("compiles")
-# def win_4x4(self):
-#     """4x4 board: make sure game is winnable"""
-#     moves = [
-#         "4", "5", "6", "1", "2", "4", "5", "6", "1", "2",
-#         "3", "7", "11", "10", "9", "1", "2", "3", "4", "5",
-#         "6", "8", "1", "2", "3", "4", "7", "11", "10", "9",
-#         "14", "13", "12", "1", "2", "3", "4", "14", "13",
-#         "12", "1", "2", "3", "4", "14", "13", "12", "1",
-#         "2", "3", "4", "12", "9", "15", "1", "2", "3", "4",
-#         "12", "9", "13", "14", "9", "13", "14", "7", "5", "9",
-#         "13", "14", "15", "10", "11", "5", "9", "13", "7", "11",
-#         "5", "9", "13", "7", "11", "15", "10", "5", "9", "13", "15",
-#         "11", "8", "6", "7", "8", "14", "12", "6", "7", "8", "14",
-#         "12", "6", "7", "8", "14", "15", "11", "10", "6", "7", "8",
-#         "12", "15", "11", "10", "15", "11", "14", "12", "11", "15",
-#         "10", "14", "15", "11", "12"
-#     ]
-#
-#     child = self.spawn("./fifteen 4").stdout("15-14-13-12|11-10-9-8|7-6-5-4|3-2-1-0\n") \
-#                                      .stdout("Tile to move:")
-#     for move in moves[:-1]:
-#         child.stdin(move).stdout("Tile to move:")
-#     child.stdin(moves[-1]).stdout("1-2-3-4|5-6-7-8|9-10-11-12|13-14-15-0").exit(0)
+
+@check50.check(compiles)
+def solve3():
+    """solves a 3x3 board."""
+    steps = ["3","4","1","2","5","8","7","6",
+             "4","1","2","5","8","7","6","4",
+             "1","2","4","1","2","3","5","4",
+             "7","6","1","2","3","7","4","8",
+             "6","4","8","5","7","8","5","6",
+             "4","5","6","7","8","6","5","4",
+             "7","8"]
+
+    board = ["1", "2", "3",
+             "4", "5", "6",
+             "7", "8", "[_0]"]
+
+    check = check50.run("./fifteen 3")
+
+    for step in steps:
+        check.stdin(step, prompt=False)
+
+    for tile in board:
+        check.stdout("tile")
+    check.stdout("\n")
+
+@check50.check(compiles)
+def solve4():
+    """solves a 4x4 board."""
+    steps = ["4","5","6","1","2","4","5","6",
+             "1","2","3","7","11","10","9","1",
+             "2","3","4","5","6","8","1","2",
+             "3","4","7","11","10","9","14",
+             "13","12","1","2","3","4","14",
+             "13","12","1","2","3","4","14",
+             "13","12","1","2","3","4","12",
+             "9","15","1","2","3","4","12","9",
+             "13","14","9","13","14","7","5",
+             "9","13","14","15","10","11","5",
+             "9","13","7","11","5","9","13",
+             "7","11","15","10","5","9","13",
+             "15","11","8","6","7","8","14",
+             "12","6","7","8","14","12","6",
+             "7","8","14","15","11","10","6",
+             "7","8","12","15","11","10","15",
+             "11","14","12","11","15","10",
+             "14","15","11","12"]
+
+    board = ["1", "2", "3", "4",
+             "5", "6", "7", "8",
+             "9", "10", "11", "12",
+             "13", "14", "15", "[_0]"]
+
+    check = check50.run("./fifteen 4")
+
+    for step in steps:
+        check.stdin(step, prompt=False)
+
+    for tile in board:
+        check.stdout("tile")
+    check.stdout("\n")
