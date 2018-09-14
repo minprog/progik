99ab927807d2b9d3cdbe85cd37c79a072b03cf3e
jelleas <jelle.van.assema@gmail.com>
2018-09-14 17:37:28 +0200

    find checks

--- find/less/__init__.py
+++ find/less/__init__.py
@@ -32,55 +32,66 @@ def first_among_three():
     """finds 28 in {28,29,30}"""
     check50.run("./find 28").stdin("28").stdin("29").stdin("30").stdin(check50.EOF).exit(0)
 
-# @check("compiles")
-# def second_among_three(self):
-#     """finds 28 in {27,28,29}"""
-#     self.spawn("./find 28").stdin("27").stdin("28").stdin("29").stdin(EOF).exit(0)
-#
-# @check("compiles")
-# def third_among_three(self):
-#     """finds 28 in {26,27,28}"""
-#     self.spawn("./find 28").stdin("26").stdin("27").stdin("28").stdin(EOF).exit(0)
-#
-# @check("compiles")
-# def second_among_four(self):
-#     """finds 28 in {27,28,29,30}"""
-#     self.spawn("./find 28").stdin("27").stdin("28").stdin("29").stdin("30").stdin(EOF).exit(0)
-#
-# @check("compiles")
-# def third_among_four(self):
-#     """finds 28 in {26,27,28,29}"""
-#     self.spawn("./find 28").stdin("26").stdin("27").stdin("28").stdin("29").stdin(EOF).exit(0)
-#
-# @check("compiles")
-# def fourth_among_four(self):
-#     """finds 28 in {25,26,27,28}"""
-#     self.spawn("./find 28").stdin("25").stdin("26").stdin("27").stdin("28").stdin(EOF).exit(0)
-#
-# @check("compiles")
-# def not_among_three(self):
-#     """doesn't find 28 in {25,26,27}"""
-#     self.spawn("./find 28").stdin("25").stdin("26").stdin("27").stdin(EOF).exit(1)
-#
-# @check("compiles")
-# def not_among_four(self):
-#     """doesn't find 28 in {25,26,27,29}"""
-#     self.spawn("./find 28").stdin("25").stdin("26").stdin("27").stdin("29").stdin(EOF).exit(1)
-#
-# @check("compiles")
-# def needle_too_low_four(self):
-#     """doesn't find 28 in {29,30,31,32}"""
-#     self.spawn("./find 28").stdin("29").stdin("30").stdin("31").stdin("32").stdin(EOF).exit(1)
-#
-# @check("compiles")
-# def needle_too_low_three(self):
-#     """doesn't find 28 in {29, 30, 31}"""
-#     self.spawn("./find 28").stdin("29").stdin("30").stdin("31").stdin(EOF).exit(1)
-#
-# @check("compiles")
-# def correctly_sorts(self):
-#     """finds 28 in {30,27,28,26}"""
-#     self.spawn("./find 28").stdin("30").stdin("27").stdin("28").stdin("26").stdin(EOF).exit(0)
+
+@check(compiles)
+def second_among_three():
+    """finds 28 in {27,28,29}"""
+    check50.run("./find 28").stdin("27").stdin("28").stdin("29").stdin(check50.EOF).exit(0)
+
+
+@check(compiles)
+def third_among_three():
+    """finds 28 in {26,27,28}"""
+    check50.run("./find 28").stdin("26").stdin("27").stdin("28").stdin(check50.EOF).exit(0)
+
+
+@check(compiles)
+def second_among_four():
+    """finds 28 in {27,28,29,30}"""
+    check50.run("./find 28").stdin("27").stdin("28").stdin("29").stdin("30").stdin(check50.EOF).exit(0)
+
+
+@check(compiles)
+def third_among_four():
+    """finds 28 in {26,27,28,29}"""
+    check50.run("./find 28").stdin("26").stdin("27").stdin("28").stdin("29").stdin(check50.EOF).exit(0)
+
+
+@check(compiles)
+def fourth_among_four():
+    """finds 28 in {25,26,27,28}"""
+    check50.run("./find 28").stdin("25").stdin("26").stdin("27").stdin("28").stdin(check50.EOF).exit(0)
+
+
+@check(compiles)
+def not_among_three():
+    """doesn't find 28 in {25,26,27}"""
+    check50.run("./find 28").stdin("25").stdin("26").stdin("27").stdin(check50.EOF).exit(1)
+
+
+@check(compiles)
+def not_among_four():
+    """doesn't find 28 in {25,26,27,29}"""
+    check50.run("./find 28").stdin("25").stdin("26").stdin("27").stdin("29").stdin(check50.EOF).exit(1)
+
+
+@check(compiles)
+def needle_too_low_four():
+    """doesn't find 28 in {29,30,31,32}"""
+    check50.run("./find 28").stdin("29").stdin("30").stdin("31").stdin("32").stdin(check50.EOF).exit(1)
+
+
+@check(compiles)
+def needle_too_low_three():
+    """doesn't find 28 in {29, 30, 31}"""
+    check50.run("./find 28").stdin("29").stdin("30").stdin("31").stdin(check50.EOF).exit(1)
+
+
+@check(compiles)
+def correctly_sorts():
+    """finds 28 in {30,27,28,26}"""
+    check50.run("./find 28").stdin("30").stdin("27").stdin("28").stdin("26").stdin(check50.EOF).exit(0)
+
 
 def test_sorted(items):
     check = check50.run("./sort")
