cf8ef49825ab00c66f38ee04956de57ee176f0d5
jelleas <jelle.van.assema@gmail.com>
2018-10-04 10:36:33 +0200

    top5

--- tweet/__init__.py
+++ tweet/__init__.py
@@ -44,6 +44,41 @@ def correct_neutral():
     check_classify("neutral", 450)
 
 
+@check50.check(compiles)
+def best_words():
+    """correct top five positive words"""
+    positive_word = uva.check50.py.run("tweet.py").module.positive_word
+
+    import helpers
+    dates, tweets = helpers.read_tweets("trump.txt")
+    positives = helpers.read_words("positive_words.txt")
+
+    with uva.check50.py.capture_stdout() as stdout:
+        positive_word(tweets, positives)
+
+    out = stdout.getvalue()
+    top5 = ["great", "trump", "thank", "good", "honor"]
+
+    for word in top5:
+        if word not in out:
+            raise check50.Mismatch(f"{word}", out)
+
+    for word in set(positives) - set(top5) - {"positive", "top"}:
+        if word and word in out:
+            raise check50.Failure(f"Did not expect {word} in top 5!")
+
+    return out
+
+
+@check50.check(best_words)
+def occurance_words(out):
+    """correct occurance of top five positive words"""
+    for word, occ in [("great", 245), ("trump", 88), ("thank", 82), ("good", 55), ("honor", 39)]:
+        match = re.search(f"{word}[^\n^\d]*(\d+)", out)
+        if not match.groups() or not int(match.groups()[0]) == occ:
+            raise check50.Mismatch(f"{word} {occ}", out)
+
+
 def check_classify(type, n):
     classify = uva.check50.py.run("tweet.py").module.classify
 
@@ -57,7 +92,7 @@ def check_classify(type, n):
         classify(tweets, positives, negatives)
 
     out = stdout.getvalue()
-    match = re.search(fr"{type}[ ]*:?[ ]*(\d+)", out)
+    match = re.search(fr"{type}[^\n^\d]*(\d+)", out)
 
     if not match or int(match.groups()[0]) != n:
         raise check50.Mismatch(f"{type}: {n}", out)
