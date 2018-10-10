fa0476378efd0e00c823682eacd28b24c68edd1b
jelleas <jelle.van.assema@gmail.com>
2018-10-10 17:49:00 +0200

    kill checks

--- virus/__init__.py
+++ virus/__init__.py
@@ -47,7 +47,7 @@ def generate_virus_elements():
     """generateVirus() produces viruses consisting only of A, T, G and C"""
     generateVirus = uva.check50.py.run("virus.py").module.generateVirus
 
-    pairs = "".join([generateVirus(10) for _ in range(1000)])
+    pairs = "".join([generateVirus(10) for _ in range(100)])
 
     if any([el not in "AGTC" for el in pairs]):
         raise check50.Failure("expected generateVirus() to return only combinations of AGTC")
@@ -72,7 +72,7 @@ def mutate_elements():
     """mutate() produces viruses consisting only of A, T, G and C"""
     virus = uva.check50.py.run("virus.py").module
 
-    pairs = "".join([virus.mutate(virus.generateVirus(10)) for _ in range(1000)])
+    pairs = "".join([virus.mutate(virus.generateVirus(10)) for _ in range(100)])
 
     if any([el not in "AGTC" for el in pairs]):
         raise check50.Failure("expected mutate() to return only combinations of AGTC")
@@ -89,3 +89,44 @@ def mutate_one_difference():
         mutated_v = virus.mutate(v)
         if not off_by_one(mutated_v, v):
             raise check50.Failure(f"expected mutate({v}) to return a virus with only one mutation, not {mutated_v}")
+
+
+@check50.check(generate_virus_elements)
+def kill_no_modify():
+    """kill() does not modify the list of viruses it accepts as argument"""
+    virus = uva.check50.py.run("virus.py").module
+
+    if not isinstance(virus.kill(["AAAA"], 0.25), list):
+        raise check50.Failure("expected kill() to return a list")
+
+    viruses = [virus.generateVirus(4) for i in range(10)]
+    viruses_copy = viruses[:]
+    new_viruses = virus.kill(viruses, 1)
+
+    if viruses != viruses_copy:
+        raise check50.Failure()
+
+
+@check50.check(kill_no_modify)
+def kill_no_new():
+    """kill() does not produce any new viruses"""
+    virus = uva.check50.py.run("virus.py").module
+
+    viruses = [virus.generateVirus(4) for i in range(100)]
+    new_viruses = virus.kill(viruses, 0.25)
+
+    if set(new_viruses).difference(set(viruses)) or len(new_viruses) > len(viruses):
+        raise check50.Failure()
+
+
+@check50.check(kill_no_modify)
+def kill_enough():
+    """kill() kills enough viruses according to mortality probability"""
+    virus = uva.check50.py.run("virus.py").module
+
+    viruses = [virus.generateVirus(4) for i in range(100)]
+
+    avg_pop_size = sum(len(virus.kill(viruses[:], 0.25)) for i in range(1000)) / 1000
+
+    if not 70 <= avg_pop_size <= 80:
+        raise check50.Failure(f"expected roughly 25% of the population to die with mortalityProb of 0.25, but {100 - avg_pop_size}% died!")
